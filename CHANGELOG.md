ceb983a74e3f1fbdc25d9c35682e288ea6000c6d
mmsqe <tqd0800210105@gmail.com>
2025-10-28 23:10:45 +0800

    fix: avoid panic on close if evm mempool not used (#772)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 ### BUG FIXES
 
 - [\#748](https://github.com/cosmos/evm/pull/748) Fix DynamicFeeChecker in Cosmos ante handler to respect NoBaseFee feemarkets' parameter.
+- [\#772](https://github.com/cosmos/evm/pull/772) Avoid panic on close if evm mempool not used.
 
 ## v0.5.0
 
--- evmd/app.go
+++ evmd/app.go
@@ -1080,7 +1080,7 @@ func (app *EVMD) SetClientCtx(clientCtx client.Context) { // TODO:VLAD - Remove 
 // Close unsubscribes from the CometBFT event bus (if set) and closes the mempool and underlying BaseApp.
 func (app *EVMD) Close() error {
 	var err error
-	if m, ok := app.GetMempool().(*evmmempool.ExperimentalEVMMempool); ok {
+	if m, ok := app.GetMempool().(*evmmempool.ExperimentalEVMMempool); ok && m != nil {
 		app.Logger().Info("Shutting down mempool")
 		err = m.Close()
 	}
